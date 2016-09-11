c49197eea8faf7345e7ce67a4451d637f31fa51c
Bibek <bibek.thapahere@gmail.com>
2016-09-10 21:32:21 -0700

    Split Command test

--- src/com/example/test/program1.java
+++ src/com/example/test/program1.java
@@ -2,7 +2,10 @@
 package com.example.test;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,60 +21,40 @@ public class program1 {
     
     public static void main(String[] args) {
        
+        String fi="C:/Users/HOME/Desktop/java/down.txt";
         
      //Buffered Reader is used for reading characters , arrays for effiecient reading of characters ,strings
      //An InputStreamReader is a bridge from byte streams to character streams:
      //It reads bytes and translates them into characters according to a specified character encoding.  
-     
-     BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-     String line ="";
-     try {
-         while(true)
-          {
-                System.out.println("Enter the URL to download");
-           if(!(line=reader.readLine()).equalsIgnoreCase("exit")){
+       try{ 
+         BufferedReader reader =new BufferedReader(new FileReader(fi));
+           String store=" ";
+          
+           while((store=reader.readLine())!=null)
+           {
+                String tokens[]=store.split("www");
+                for(String i:tokens)
+                {
+                    System.out.println(i);
+                }
+                System.out.println(tokens.length);
                
-               URL url =new URL(line);
-               URLConnection conn=url.openConnection();
-               String[] tokens=line.split("/");
-               byte b[]=new byte[1024];
-               //InputStream class is the superclass of all classes representing an input stream of bytes.
-               InputStream is=conn.getInputStream();
-               
-               //Now we have to write Input stream part in file
-               FileOutputStream os=new FileOutputStream(tokens[tokens.length-1]);
-               int i=0;
-               while((i=is.read(b))!=-1)
+                System.out.println(store);
+                
+                
+              
+           
+           }
+          
+           reader.close();
+           
+          
+           }catch(IOException ioe)
                {
-              os.write(b, 0, i);
+                   ioe.printStackTrace();
+                   System.out.println("I am in IOException");
                
                }
-               
-               
-               
-               
-            System.out.println(line);}
-          
-          else
-          {
-                System.exit(0);
-          }
-          
-          }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            System.out.println("I am in IOE exception");
-        }
-     
-       
-        
-   
     
-    
-    
-    }
-
-   
-        
-        
+    }       
 }
